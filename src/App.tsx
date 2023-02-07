4a2f107e5a3e22313bb67f3493aa5a737c4e6788
Maghfoor Ahmed <maghfoor.ahmed05@gmail.com>
2023-02-07 16:29:32 +0000

    added GameBoard component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import GameBoard from "./components/GameBoard";
+import "./styles.css"
+
 function App() {
   return (
     <>
-      <h1>Vite React TypeScript Template!</h1>
+      <GameBoard />
     </>
   );
 }
